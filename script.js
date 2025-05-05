ae5409f8d2cc5c6ccaac8f148adbcdaaf048ba62
BaboJunYoung <junyoung090115@gmail.com>
2025-05-05 15:46:31 +0900

    키 입력

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let cellNumber = [1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+let cellNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
 cell = document.getElementsByClassName("block");
 
@@ -7,4 +7,22 @@ function display() {
         cell[i].innerHTML = "<div>" + cellNumber[i]+"</div>"
     }
 }
-display();
+function keydown(event) {
+    console.log(event)
+    if (event.keyCode == "38" || event.keyCode == "87") {
+        // up
+
+    }
+    else if (event.keyCode == "40" || event.keyCode == "83") {
+        // down
+    }
+    else if (event.keyCode == "37" || event.keyCode == "65") {
+        // left
+    }
+    else if (event.keyCode == "39" || event.keyCode == "68") {
+        // right
+    }
+}
+/* display(); */
+
+document.addEventListener("keydown", keydown)
